588494d2905c16e1e6310fef8a70dcc7a9c862a9
CamTheBeast <75223349+CamTheBeast@users.noreply.github.com>
2024-12-02 19:46:06 -0800

    Create server.c

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#define PortNumber
+#define MaxConnects
+#define BuffSize
+#define ConversationLen
+#define Host
